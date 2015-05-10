f8e997aabc53f7f94113a8f2b3290c9b73accf7f
Rahul Gupta <rahul11061995@gmail.com>
2015-05-10 19:50:30 +0530

    Initial Admin Panel Commit

--- db/migrate/20150225204917_create_users_admin_users_join.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsersAdminUsersJoin < ActiveRecord::Migration
-  def change
-    create_table :admin_users_users do |t|
-    	t.references :admin_user
-    	t.references :user
-    end
-    add_index("admin_users_users",["admin_user_id","user_id"])
-  end
-end
